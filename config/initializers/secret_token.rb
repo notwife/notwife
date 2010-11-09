101145f6c4ea6c7b9f33d5873325e205e40ec5eb
Kei Shiratsuchi <kei-s@mbc.nifty.com>
2010-11-09 17:31:59 +0900

    rails new notwife -T -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notwife::Application.config.secret_token = '024c8b4e39c952f7add691c9a9f997e0e889f3ffdcadcad57543c4882556dc90577be581f03023bbe7756a86dcae0ce17736d8e4c1859243bfcaaca3376f10ea'
