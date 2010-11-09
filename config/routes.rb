4b2e93fb0f639ef7fd63f67f92b7ed0c35375aea
Kei Shiratsuchi <kei-s@mbc.nifty.com>
2010-11-10 03:24:00 +0900

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Notwife::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match '/auth/:provider/callback', :to => 'sessions#create'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
