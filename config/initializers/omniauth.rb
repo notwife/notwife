4aa5769f688b85fd017fbb0c1942c406890258fd
Kei Shiratsuchi <kei-s@mbc.nifty.com>
2010-11-12 02:34:44 +0900

    fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
-consumer_key = configatron.twitter.consumer_key || ENV['TWITTER_CONSUMER_KEY']
-consumer_secret = configatron.twitter.consumer_secret || ENV['TWITTER_CONSUMER_SECRET']
+consumer_key = configatron.twitter.consumer_key.blank? ? ENV['TWITTER_CONSUMER_KEY'] : configatron.twitter.consumer_key
+consumer_secret = configatron.twitter.consumer_secret.blank? ? ENV['TWITTER_CONSUMER_SECRET'] : configatron.twitter.consumer_secret
 
-raise 'Twitter API settings is not found. Add config/config.yml OR ENV["TWITTER_CONSUMER_KEY"] and ENV["TWITTER_CONSUMER_SECRET"]' if consumer_key.nil? || consumer_secret.nil?
+raise 'Twitter API settings is not found. Add config/config.yml OR ENV["TWITTER_CONSUMER_KEY"] and ENV["TWITTER_CONSUMER_SECRET"]' unless !consumer_key.nil? && !consumer_secret.nil?
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, consumer_key, consumer_secret
