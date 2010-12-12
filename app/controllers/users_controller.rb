24bc61a55fe7f8babb3a5dadbe68cef1bb3486cb
june29 <june29.jp@gmail.com>
2010-12-13 08:00:24 +0900

    add 'return' to prevent unintended process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@
 class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
-    redirect_to root_path unless @user == current_user
+
+    unless @user == current_user
+      redirect_to root_path
+      return
+    end
 
     @user.notifo_username = params[:user][:notifo_username]
     if @user.save
@@ -17,7 +21,11 @@ class UsersController < ApplicationController
 
   def sample
     @user = User.find(params[:id])
-    redirect_to root_path unless @user == current_user
+
+    unless @user == current_user
+      redirect_to root_path
+      return
+    end
 
     title = "information"
     text  = "Success!"
