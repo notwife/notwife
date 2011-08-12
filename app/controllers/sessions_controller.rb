6bc443025b8a93e8f3fd29415ff2dadfd5ca0707
june29 <june29.jp@gmail.com>
2011-08-13 03:56:58 +0900

    change hash key

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    auth = request.env['rack.auth']
+    auth = request.env['omniauth.auth']
     unless @auth = Authorization.find_from_hash(auth)
       @auth = Authorization.create_from_hash(auth, current_user)
     end
